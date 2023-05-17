1b47771a3535358ae26e28f815c8a753d2a64a2b
Daniel <thedanils63@mail.ru>
2023-05-17 13:43:25 +0300

    added gateway

--- inventory-service/src/main/java/ru/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/ru/inventoryservice/InventoryServiceApplication.java
@@ -3,11 +3,13 @@ package ru.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import ru.inventoryservice.model.Inventory;
 import ru.inventoryservice.repositoory.InventoryRepository;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
