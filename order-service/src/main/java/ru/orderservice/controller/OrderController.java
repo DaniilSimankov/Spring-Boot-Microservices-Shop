73d37b87993f59ea403f0aa222bcb525b77d38bd
Daniel <thedanils63@mail.ru>
2023-05-23 21:39:35 +0300

    implements Circuit Breaker and Distributed Tracing

--- order-service/src/main/java/ru/orderservice/controller/OrderController.java
+++ order-service/src/main/java/ru/orderservice/controller/OrderController.java
@@ -1,22 +1,35 @@
 package ru.orderservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.orderservice.dto.OrderRequest;
 import ru.orderservice.service.OrderService;
 
+import java.util.concurrent.CompletableFuture;
+
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeHolder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        return "Order Placed Successfully";
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeHolder(@RequestBody OrderRequest orderRequest) {
+        return CompletableFuture.supplyAsync(()->orderService.placeOrder(orderRequest)); // делегируется на другой поток, основной не блокируется и продолжает работу
+    } // также в Zipkin это будет как другой request => разные trace
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some time!");
     }
 }
--- order-service/src/main/java/ru/orderservice/service/OrderService.java
+++ order-service/src/main/java/ru/orderservice/service/OrderService.java
@@ -1,6 +1,10 @@
 package ru.orderservice.service;
 
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,12 +22,15 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final ObservationRegistry observationRegistry;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
-    public void placeOrder(OrderRequest orderRequest) {
+    public String placeOrder(OrderRequest orderRequest) {
         Order order = Order.builder()
                 .orderNumber(UUID.randomUUID().toString())
                 .orderLineItemsList(orderRequest.getOrderLineItemsDtoList()
@@ -34,22 +41,33 @@ public class OrderService {
 
         List<String> skuCodes = order.getOrderLineItemsList()
                 .stream()
-                .map(OrderLineItems::getSkuCode).toList();
+                .map(OrderLineItems::getSkuCode)
+                .toList();
 
-        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
-                .uri("http://inventory-service/api/inventory",
-                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class)
-                .block();
+        //Call inventory service, and place order if product is in stock
+        Observation inventoryServiceObservation = Observation.createNotStarted("inventory-service-lookup", this.observationRegistry);
+        inventoryServiceObservation.lowCardinalityKeyValue("call", "inventory-service");
 
-        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
-                .allMatch(InventoryResponse::isInStock);
+        return inventoryServiceObservation.observe(() -> {
 
-        if (Boolean.TRUE.equals(allProductsInStock)) {
-            orderRepository.save(order);
-        } else {
-            throw new IllegalArgumentException("Some product is not in stock, please try again lates");
-        }
+            InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
+
+            boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+                    .allMatch(InventoryResponse::isInStock);
+
+            if (Boolean.TRUE.equals(allProductsInStock)) {
+                orderRepository.save(order);
+//                applicationEventPublisher.publishEvent(new OrderPlacedEvents);
+                return "Order place successfully";
+
+            } else {
+                throw new IllegalArgumentException("Some product is not in stock, please try again later");
+            }
+        });
     }
 }
